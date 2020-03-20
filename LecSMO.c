53c9eac5d806572a8ffdb1020795c4fa34630789
spade2344 <62391845+spade2344@users.noreply.github.com>
2020-03-20 22:05:26 +0100

    Add files via upload

---
+++ LecSMO.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "forfun.h"
+
+void LecSMO(int* NbLignO1, int* NbCoefO1, float** secmbrO1, int** AdPrCoefLiO1,
+            float** MatriceO1, int** NumColO1, char* nomficO){
+
+  FILE* smo1 = fopen(nomficO, "rb");
+  if (smo1 == NULL){
+    printf("Ouverture du fichier impossible !\n");
+    exit(1);
+  }
+
+  /* Lecture du fichier binaire*/
+  fread(NbLignO1, sizeof(int), 1, smo1 );
+
+  *AdPrCoefLiO1=malloc(*NbLignO1*sizeof(int));
+  *secmbrO1=malloc(*NbLignO1*sizeof(float));
+
+  fread( *secmbrO1, sizeof(float), *NbLignO1, smo1 );
+  fread( *AdPrCoefLiO1, sizeof(int), *NbLignO1, smo1 );
+
+  *NbCoefO1 = (*AdPrCoefLiO1)[*NbLignO1-1]-1;
+
+  *MatriceO1=malloc((*NbLignO1+*NbCoefO1)*sizeof(float));
+  *NumColO1=malloc((*NbCoefO1)*sizeof(int));
+
+  fread( *MatriceO1, sizeof(float),*NbLignO1+*NbCoefO1, smo1 );
+  fread( *NumColO1, sizeof(int), *NbCoefO1, smo1 );
+
+  fclose(smo1);
+
+}
---
+++ dSMDaSMO.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "forfun.h"
+#include "assmb.h"
+
+
+void dSMDaSMO(float** secmbrO, int** AdPrCoefLiO, float** MatriceO, int** NumColO, char* nomfich, char* nomfic){
+
+
+  /*** Lecture du fichier binaire SMD ***/
+
+  int NbLign1;
+  int* AdPrCoefLi1;
+  float* secmbr1;
+  int* nuddir1;
+  float* valdir1;
+  int NbCoef1;
+  float *Matrice1;
+  int* NumCol1;
+  int* AdSuccLi1;
+
+
+  LecSMD(&NbLign1, &NbCoef1, &secmbr1, &nuddir1, &valdir1,
+	     &AdPrCoefLi1, &Matrice1, &NumCol1, &AdSuccLi1,nomfich);
+
+  *secmbrO=(float *) calloc(NbLign1, sizeof(float));
+  *AdPrCoefLiO=(int *) calloc(NbLign1,sizeof(int));
+  *MatriceO=(float *) calloc((NbLign1+NbCoef1),sizeof(float));
+  *NumColO=malloc((NbCoef1)*sizeof(int));
+
+
+  /*** Passage du stockage SMD a SMO ***/
+
+  cdesse_(&NbLign1, AdPrCoefLi1, NumCol1, AdSuccLi1,
+          Matrice1, secmbr1, nuddir1, valdir1,
+          *AdPrCoefLiO, *NumColO, *MatriceO, *secmbrO);
+
+
+  /*** Ecriture dans le fichier binaire SMO ***/
+
+  FILE* smo = fopen(nomfic, "wb");        /* "SMO_bin.txt" */
+  if (smo == NULL){
+    printf("Ouverture du fichier impossible !\n");
+    exit(1);
+  }
+
+  fwrite(&NbLign1, sizeof(int), 1, smo );
+  fwrite( *secmbrO, sizeof(float), NbLign1, smo );
+  fwrite( *AdPrCoefLiO, sizeof(int), NbLign1, smo );
+  fwrite( *MatriceO, sizeof(float), NbLign1+NbCoef1, smo );
+  fwrite( *NumColO, sizeof(int), NbCoef1, smo );
+
+  fclose(smo);
+
+  /* Liberation memoire */
+
+  free(Matrice1);
+  free(AdPrCoefLi1);
+  free(AdSuccLi1);
+  free(NumCol1);
+  free(valdir1);
+  free(nuddir1);
+  free(secmbr1);
+
+}
